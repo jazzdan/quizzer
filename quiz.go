002c3790d4e934d4637b6d56b3e5fcfef3b0ebba
Dan Miller <danmiller@upserve.com>
2017-11-24 16:28:18 -0500

    Initial commit

---
+++ quiz.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./problems.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	numCorrect := 0
+	numIncorrect := 0
+
+	r := csv.NewReader(bufio.NewReader(file))
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		problem := record[0]
+		solution := record[1]
+
+		fmt.Printf("%v?\n", problem)
+		var response string
+		fmt.Scanln(&response)
+
+		if response == solution {
+			fmt.Print("Correct!\n")
+			numCorrect++
+		} else {
+			fmt.Print("Incorrect :(\n")
+			numIncorrect++
+		}
+	}
+
+	fmt.Printf(" Number correct: %v, number incorrect: %v\n", numCorrect, numIncorrect)
+}
