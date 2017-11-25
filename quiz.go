716fe1d80435313e731f96ac6414b6672c8fd74e
Dan Miller <danmiller@upserve.com>
2017-11-25 14:22:35 -0500

    Implement part 2

--- quiz.go
+++ quiz.go
@@ -7,9 +7,15 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
+func printResults(numCorrect int, numIncorrect int) {
+	fmt.Printf("Number correct: %v, number incorrect: %v\n", numCorrect, numIncorrect)
+}
+
 func main() {
+	finished := make(chan bool)
 	file, err := os.Open("./problems.csv")
 
 	if err != nil {
@@ -21,31 +27,45 @@ func main() {
 
 	r := csv.NewReader(bufio.NewReader(file))
 
+	go func() {
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				finished <- true
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			problem := record[0]
+			solution := record[1]
+
+			fmt.Printf("%v?\n", problem)
+			var response string
+			fmt.Scanln(&response)
+
+			if response == solution {
+				fmt.Println("Correct!")
+				numCorrect++
+			} else {
+				fmt.Println("Incorrect :(")
+				numIncorrect++
+			}
+		}
+	}()
+
+	timeLimit := time.After(30 * time.Second)
+
 	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-			continue
-		}
-
-		problem := record[0]
-		solution := record[1]
-
-		fmt.Printf("%v?\n", problem)
-		var response string
-		fmt.Scanln(&response)
-
-		if response == solution {
-			fmt.Println("Correct!")
-			numCorrect++
-		} else {
-			fmt.Println("Incorrect :(")
-			numIncorrect++
+		select {
+		case <-timeLimit:
+			fmt.Println("TIMES UP YER DONE")
+			printResults(numCorrect, numIncorrect)
+			os.Exit(0)
+		case <-finished:
+			fmt.Println("Congrats you finished!")
+			printResults(numCorrect, numIncorrect)
+			os.Exit(0)
 		}
 	}
-
-	fmt.Printf(" Number correct: %v, number incorrect: %v\n", numCorrect, numIncorrect)
 }
